2adf6834ae64f87aa0b71be732399f010f6ec9f5
Christian Bunk <christian.bunk@sensorberg.com>
2019-04-29 01:47:28 +0200

    move train and test method to Mnist class

--- Benchmark.py
+++ Benchmark.py
@@ -1,15 +1,10 @@
 from Mnist import Mnist
-import numpy
 
 # Benchmark configuration
 
-epochs = 10
-hidden_nodes_list = [100, 200, 400]  # default 100
-learning_rate_list = [0.2, 0.1, 0.05, 0.025, 0.0125]
-train_data_list = Mnist.get_train_data_list()
-test_data_list = Mnist.get_test_data_list()
-
-mnist = Mnist()
+epochs = 1
+hidden_nodes_list = [100]  # default 100
+learning_rate_list = [0.3]
 
 best_performance = 0
 best_learning_rate = 0
@@ -21,7 +16,7 @@ for h_nodes in hidden_nodes_list:
 
     for lr in learning_rate_list:
         print("  learning rate: ", lr)
-        n = mnist.create_neural_network(h_nodes, lr)
+        mnist = Mnist(h_nodes, lr)
 
         performance_list = []
 
@@ -29,55 +24,11 @@ for h_nodes in hidden_nodes_list:
             print("    run training epoch: ", e + 1)
 
             # train the neural network
-
-            for record in train_data_list:
-                all_values = record.split(',')  # split the record by the ',' commas
-
-                inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01  # scale and shift he inputs
-
-                targets = numpy.zeros(mnist.output_nodes) + 0.01
-
-                targets[int(all_values[0])] = 0.99
-
-                n.train(inputs, targets)
-                pass
+            mnist.train()
 
             # test the neural network
-
             print("    start testing...")
-
-            scorecard = []
-
-            # go through all the records in the test data set
-            for record in test_data_list:
-
-                all_values = record.split(',')  # split the record by the ',' commas
-
-                correct_label = int(all_values[0])  # correct answer is the first label
-                # print(correct_label, " is correct value")
-
-                # scale and shift the inputs
-                inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-
-                # query the network
-                outputs = n.query(inputs)
-
-                # the index of the highest value corresponds to the label
-                label = numpy.argmax(outputs)
-                # print(label, " is networks answer")
-
-                # append correct or incorrect answer to list
-                if label == correct_label:
-                    scorecard.append(1)
-                else:
-                    scorecard.append(0)
-                    pass
-
-                pass
-
-            # calculates the performance score, the fraction of correct answers
-            scorecard_array = numpy.asarray(scorecard)
-            performance = scorecard_array.sum() / scorecard_array.size
+            performance = mnist.test()
 
             if performance > best_performance:
                 best_performance = performance
--- Mnist.py
+++ Mnist.py
@@ -1,16 +1,17 @@
+import numpy
+
 import FileReader
 from NeuralNetwork import NeuralNetwork
 
 
 class Mnist:
-    def __init__(self):
+    def __init__(self, hidden_nodes, learning_rate):
         self.input_nodes = 784  # input data has 28 * 28 pixel
         self.output_nodes = 10  # output data are the numbers from 0..9
 
-    pass
-
-    def create_neural_network(self, hidden_nodes, learning_rate):
-        return NeuralNetwork(self.input_nodes, hidden_nodes, self.output_nodes, learning_rate)
+        self.train_data_list = Mnist.get_train_data_list()
+        self.test_data_list = Mnist.get_test_data_list()
+        self.neural_network = NeuralNetwork(self.input_nodes, hidden_nodes, self.output_nodes, learning_rate)
 
     pass
 
@@ -27,6 +28,56 @@ class Mnist:
         optional_test_file_name = "data/mnist_test.csv"
         default_test_file_name = "data/mnist_test_10.csv"
         return FileReader.read_optional_file_or_default(optional_test_file_name, default_test_file_name)
+
+    pass
+
+    def train(self):
+        for record in self.train_data_list:
+            all_values = record.split(',')  # split the record by the ',' commas
+
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01  # scale and shift he inputs
+
+            targets = numpy.zeros(self.output_nodes) + 0.01
+
+            targets[int(all_values[0])] = 0.99
+
+            self.neural_network.train(inputs, targets)
+            pass
+
+    pass
+
+    def test(self):
+        scorecard = []
+        for record in self.test_data_list:
+
+            all_values = record.split(',')  # split the record by the ',' commas
+
+            correct_label = int(all_values[0])  # correct answer is the first label
+            # print(correct_label, " is correct value")
+
+            # scale and shift the inputs
+            inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+
+            # query the network
+            outputs = self.neural_network.query(inputs)
+
+            # the index of the highest value corresponds to the label
+            label = numpy.argmax(outputs)
+
+            # append correct or incorrect answer to list
+            if label == correct_label:
+                scorecard.append(1)
+            else:
+                scorecard.append(0)
+            pass
+
+        pass
+
+        # calculates the performance score, the fraction of correct answers
+        scorecard_array = numpy.asarray(scorecard)
+
+        # return performance
+        return scorecard_array.sum() / scorecard_array.size
 
     pass
 
