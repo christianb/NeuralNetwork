6a38e18b730e652446f4fbc42f5e9174ece2ed7b
Christian Bunk <christian.bunk@sensorberg.com>
2019-04-28 12:40:46 +0200

    the the neural network with test data

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -137,3 +137,23 @@ for record in train_data_list:
 
     n.train(inputs, targets)
     pass
+
+# test the neural network 
+
+test_data_file = open("data/mnist_test_10.csv", 'r')
+test_data_list = test_data_file.readlines()  # do not read whole files in memory!
+test_data_file.close()
+
+# get the first test record
+all_values = test_data_list[5].split(',')
+
+# print the label
+print(all_values[0])
+
+# image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+# matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+# matplotlib.pyplot.title('Zeichen')
+# matplotlib.pyplot.show()
+
+result = n.query((numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01)
+print(result)
